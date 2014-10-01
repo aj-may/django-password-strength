1462845f55fc3301d30089a3bffc2ae9f29a968c
A.J. May <amay@northps.com>
2014-09-30 20:24:15 -0400

    aria progressbar bugfix

--- django_password_strength/static/django_password_strength/js/password_strength.js
+++ django_password_strength/static/django_password_strength/js/password_strength.js
@@ -69,12 +69,12 @@ $(function($) {
                 password_strength_info.find('.label').addClass('hidden');
             }
 
-            password_strength_bar.width( ((result.score+1)/5)*100 + '%' );
+            password_strength_bar.width( ((result.score+1)/5)*100 + '%' ).attr('aria-valuenow', result.score + 1);
             password_strength_info.find('.password_strength_time').html(display_time(result.crack_time));
             password_strength_info.removeClass('hidden');
         } else {
             password_strength_bar.removeClass('progress-bar-success').addClass('progress-bar-warning');
-            password_strength_bar.width( '0%' );
+            password_strength_bar.width( '0%' ).attr('aria-valuenow', 0);
             password_strength_info.addClass('hidden');
         }
         match_passwords($(this));
