09d6b1be84d6deefcd090772c4c5bf767b5db9b7
alexsilva <geniofuturo@gmail.com>
2019-08-15 12:00:46 -0300

    v1.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-password-strength',
-    version='1.2.1',
+    version='1.2.2',
     url='http://thegoods.aj7may.com/django-password-strength',
     author='A.J. May',
     author_email='aj7may@gmail.com',
